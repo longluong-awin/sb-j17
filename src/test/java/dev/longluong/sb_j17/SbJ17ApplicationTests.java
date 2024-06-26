85e0b2de66d93959b0cc59929660e99b070b44c9
Long Luong <long.luong@awin.com>
2024-06-26 09:32:55 +0100

    Initial commit

---
+++ src/test/java/dev/longluong/sb_j17/SbJ17ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.longluong.sb_j17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJ17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
